9a8156044ed97213a5ce5d016104e1dcd620a571
vaniog <vaniog@yandex-team.ru>
2024-10-12 15:24:28 +0300

    work

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from .health.api import router as health_router
 from .recognition.api import router as recognition_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(prefix="/health", router=health_router)
 app.include_router(prefix="/recognition", router=recognition_router)
--- app/recognition/api.py
+++ app/recognition/api.py
@@ -6,17 +6,17 @@ from fastapi import APIRouter, File, UploadFile
 from app.ml.src.predictor import Predictor
 
 router = APIRouter()
+predictor = Predictor()
 
-@router.post('/upload')
+
+@router.post("/upload")
 async def upload_voice(file: UploadFile = File(...)):
     with tempfile.TemporaryDirectory() as tmpdir:
         temp_file_path = os.path.join(tmpdir, file.filename)
-        
+
         with open(temp_file_path, "wb") as temp_file:
             shutil.copyfileobj(file.file, temp_file)
-            
-        predictor = Predictor()
-        
+
         result = predictor(temp_file_path)
-        
+
     return {"result": result}
